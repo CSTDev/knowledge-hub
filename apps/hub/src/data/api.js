5fa7af1279740c6ab08796116b404b650b39bbb7
CSTDev <pietomb00@hotmail.com>
2018-06-06 10:36:39 +0100

    Fixed record json not being right. Use env files to provide path to API.

--- apps/hub/src/data/api.js
+++ apps/hub/src/data/api.js
@@ -2,22 +2,14 @@ import React from 'react';
 
 export function CreateRecord(record) {
     console.log("Create called")
-    console.dir(record)
-    console.log(JSON.stringify({
-        record
-    }))
+
     try{
-
-        
-        //TODO It's passing it as {"record":{"title": "a Title"}} etc. so service doesn't recognise it.
-
-
-        return fetch('http://localhost:8000/v1/record', {
+        return fetch(process.env.REACT_APP_API_URL + '/record', {
             method:'POST',
             headers: {'Content-Type':'application/json'},
-            body: JSON.stringify({
+            body: JSON.stringify(
                 record
-            })
+            )
         }).then(response => {
             return response
         })
